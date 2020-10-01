86603cb5f2ca6278e42a47c6273342fd856377a7
repl.it user <replituser@example.com>
2020-10-01 09:05:23 +0000

    linkin park reference

--- basics/output/simple_message.py
+++ basics/output/simple_message.py
@@ -1,4 +1,8 @@
 # Display message to the standard output
 print("System Failure Imminent!")
 #Display message to the standard output
-print("System reboot has been initiated...")
+print("System reboot has been initiated...\n")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online.")
